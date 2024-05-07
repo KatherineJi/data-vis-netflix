e1b505e0e15a9fd1e8dd86383250120f558b0d77
墨墨 <momo@s-MacBook-Pro-3.local>
2024-05-07 12:32:26 +0800

    feat: update

--- src/app/components/chartMap/index.tsx
+++ src/app/components/chartMap/index.tsx
@@ -19,10 +19,12 @@ echarts.use([
   BarChart
 ]);
 
+const colorSet = ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026'];
+
 function getColor(value: number) {
-  const index = Math.floor((value - 0) / 1000 * 3);
-  return ['#fee8c8','#fdbb84','#e34a33'][index] || '#e34a33';
-  // ['blue', '#96ff00', '#aeff00', '#ccff00', '#e4ff00', '#fcff00', '#ffde00', '#ffb400', '#ff9000', '#ff5a00', '#ff3000', 'red'][index] || 'red';
+
+  const index = Math.floor((value - 0) / 1000 * (colorSet.length + 1));
+  return colorSet[index] || '#a50026';
 }
 
 const ChartMap: React.FC = () => {
@@ -196,11 +198,12 @@ const ChartMap: React.FC = () => {
           left: 'right',
           // categories: data.map((item) => item.name),
           min: 0,
-          max: 1000,
+          max: 4000,
           // range: [0, 450],
           inRange: {
             // prettier-ignore
-            color: ['#fee8c8','#fdbb84','#e34a33'],
+            color: colorSet,
+            // color: ['#fee8c8','#fdbb84','#e34a33'],
             // color: ['blue', '#96ff00', '#aeff00', '#ccff00', '#e4ff00', '#fcff00', '#ffde00', '#ffb400', '#ff9000', '#ff5a00', '#ff3000', 'red'],
             // color: { 'United States': 'red', 'India': 'yellow' }
             // color: ['blue', 'yellow', 'yellow', 'red', 'red', 'red', 'red']
